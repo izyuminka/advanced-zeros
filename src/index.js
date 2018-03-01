ec31353da7d9eed194fb43ca44590187907e57dd
Sviatlana Kuraptsova <kuraptsova.s@gmail.com>
2018-03-01 01:42:52 +0100

    [advanced-zeros]/feature: calculated multipliers for base, checked multipliers occurrence in number, validated against the most often occurring multiplier.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+    let multpliersAndItsCount = [];
+    let multiplier = 2;
+    let value = base;
+    while (multiplier <= value) {
+        if (value % multiplier === 0) {
+            value = value / multiplier;
+            let multiplierCount = multpliersAndItsCount.filter((e) => e[0] === multiplier);
+            if (multiplierCount.length !== 0) {
+                multiplierCount[0][1]++;
+            } else {
+                multpliersAndItsCount.push([multiplier, 1]);
+            }
+            multiplier = 2;
+        } else {
+            multiplier++;
+        }
+    }
+    let countNumOfMultipliers = (v, d) => {
+        let r = (v - v % d) / d;
+        if (r >= d) {
+            r += countNumOfMultipliers(r, d);
+        }
+        return r;
+    };
+    let numbers = multpliersAndItsCount.map((e) => {
+        let count = countNumOfMultipliers(number, e[0]);
+        return (count - count % e[1]) / e[1];
+    });
+    numbers.sort((a, b) => {
+        return a - b;
+    });
+    return numbers[0];
+};
